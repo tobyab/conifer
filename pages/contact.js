041b0335a227310d508b8073d137c0e747712e1b
Toby <mrtobez08@gmail.com>
2022-02-17 11:29:44 +0000

    changes made

--- pages/contact.js
+++ pages/contact.js
@@ -10,6 +10,7 @@ export default function Home() {
       <Head>
         <title>Site kit</title>
         <meta name="description" content="Contact page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <Header></Header>
